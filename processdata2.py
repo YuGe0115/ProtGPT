0b1bc496e378a247b3d6937e9d797fda242918f6
YuGe0115 <1724905827@qq.com>
2025-08-19 10:09:18 +0800

    Update processdata2.py

--- processdata2.py
+++ processdata2.py
@@ -13,12 +13,14 @@ print(f"最大序列长度: {max_length}, 平均序列长度: {sum(lengths)/len(
 train_validtest = dataset["train"].train_test_split(test_size=0.3)
 valid_test = train_validtest["test"].train_test_split(test_size=0.5)
 
+tokenizer = AutoTokenizer.from_pretrained("/home/tongyi/protgpt/protgpt3")
+if "<PAD>" not in tokenizer.get_vocab():
+    tokenizer.add_special_tokens({"pad_token": "<PAD>"})
+tokenizer.pad_token = "<PAD>"
+
 # 离线加载分词器
 tokenizer = AutoTokenizer.from_pretrained("/home/tongyi/protgpt/protgpt3")
-# 设置 padding token 为 <|endoftext|>，避免缺失报错
-if tokenizer.pad_token is None:
-    tokenizer.pad_token = tokenizer.eos_token
-    print("已设置 pad_token 为 eos_token: ", tokenizer.pad_token)
+tokenizer.pad_token = "<PAD>"  # 自定义填充 token
 
 # 创建 DatasetDict
 dataset = DatasetDict({
@@ -29,7 +31,7 @@ dataset = DatasetDict({
 
 # Tokenize 函数
 def tokenize_function(examples):
-    return tokenizer(examples["text"], truncation=True, max_length=max_length, padding="max_length")
+    return tokenizer(examples["text"], truncation=True, max_length=max_length, padding="max_length",add_special_tokens=True)
 
 # 应用 tokenize
 tokenized_dataset = DatasetDict({
@@ -38,12 +40,17 @@ tokenized_dataset = DatasetDict({
     "test": dataset["test"].map(tokenize_function, batched=True, remove_columns=["text"])
 })
 
+for i in range(min(5, len(tokenized_dataset["train"]))):
+    input_ids = tokenized_dataset["train"][i]["input_ids"]
+    print(f"训练集序列 {i+1} input_ids 长度: {len(input_ids)}")
+    print(f"解码序列: {tokenizer.decode(input_ids)}")
+
 # 验证数据集内容
 for i in range(min(5, len(tokenized_dataset["train"]))):
     print(f"训练集序列 {i+1} input_ids 长度: {len(tokenized_dataset['train'][i]['input_ids'])}")
 
 # 保存数据集
-tokenized_dataset.save_to_disk("/home/tongyi/protgpt/tokenized_antibody_dataset")
+tokenized_dataset.save_to_disk("/home/tongyi/protgpt/tokenized_antibody_dataset2")
 
 print("数据集准备完成:", tokenized_dataset)
 print("训练集大小:", len(tokenized_dataset["train"]))
